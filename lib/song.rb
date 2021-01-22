f0e014c610fb48d258fc95168654d54330bd4b0e
jinook929 <jinook929@gmail.com>
2021-01-21 22:29:03 -0500

    commit tree 4abf8012204504f2f473ca1fbd716186b1dd1433

--- lib/song.rb
+++ lib/song.rb
@@ -6,8 +6,63 @@ class Song
     @@all
   end
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name=(name)
+    song
+  end
+
+  def self.create_by_name(name)
+    song = new_by_name(name)
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if !self.find_by_name(name)
+      self.create_by_name(name)
+    else
+      self.find_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+    song_info = filename.gsub(".mp3", "").split(" - ")
+    name = song_info[1]
+    p "Song Name: #{name}"
+    artist_name = song_info[0]
+    p "Artist Name: #{artist_name}"
+    song = Song.new
+    song.name = name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.create_from_filename(filename)
+    song = self.new_from_filename(filename)
+    song.save
+  end
+
   def save
     self.class.all << self
+  end
+
+  def self.destroy_all
+    self.all.clear
   end
 
 end
